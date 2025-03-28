f0a770b8e933fcd31ac842969f73d4496b4e00cd
Elisabeth <de-cambourg.elisabeth@orange.fr>
2025-03-28 15:01:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/OC_Projet5",
+  base: "/OC_Projet5/",
   // base: "/",
   plugins: [react()],
 })
